4f27c01a823cc2ec361e21a1960ea19e982b7691
Guilherme Faccin Huth <guilherme.faccin-huth@grenoble-inp.org>
2025-07-09 23:09:16 -0300

    Filtragem funcionando

--- ESP32-ADC-Plotter-Demo-master/esp32_fast_plot_demo/src/main.cpp
+++ ESP32-ADC-Plotter-Demo-master/esp32_fast_plot_demo/src/main.cpp
@@ -1,73 +1,56 @@
 #include <Arduino.h>
-// #include <WebSocketsServer.h>
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
 #include "SPIFFS.h"
 #include <driver/i2s.h>
 #include <driver/adc.h>
 #include "esp_task_wdt.h"
-
-
-//SSID and Password of your WiFi router
 #include "my_wifi.h"  
-///// my_wifi.h contains the following lines with my own wifi info.
-///// You can remove the line above and uncomment the lines below with your info,
-///// or make your own my_wifi.h containing the following lines with your info.
-///// I had to do it this way in order to safely upload to github.
-/*
-const char* ssid = "my_wifi_ssid";
-const char* password = "my_wifi_pw";
-*/
 
+// ADC CONFIG
+i2s_port_t i2s_port = I2S_NUM_0;
+adc1_channel_t adc_channel = ADC1_CHANNEL_7; // GPIO35
+const uint16_t adc_sample_freq = 800;      // mudou pra 1000
 
-/** ADC CONFIG **/
-i2s_port_t i2s_port = I2S_NUM_0;   // I2S Port 
-adc1_channel_t adc_channel = ADC1_CHANNEL_7; //GPIO35  (SET THIS FOR YOUR HARDWARE)
-const uint16_t adc_sample_freq = 10000;
 const uint16_t dma_buffer_len = 1024;
 const uint16_t i2s_buffer_len = dma_buffer_len;
 const uint16_t ws_tx_buffer_len = dma_buffer_len;
+
 uint16_t* i2s_read_buff = (uint16_t*)calloc(i2s_buffer_len, sizeof(uint16_t));
-uint16_t* ws_send_buffer = (uint16_t*)calloc(ws_tx_buffer_len, sizeof(uint16_t));
+char ws_send_buffer[100];  // buffer para texto de envio via WS
 
-
-AsyncWebServer    server(80);
-// WebSocketsServer  webSocket = WebSocketsServer(81);
-AsyncEventSource  events("/events");
-AsyncWebServerRequest *request;
-AsyncWebSocket    ws("/test");
+AsyncWebServer server(80);
+AsyncWebSocket ws("/test");
 AsyncWebSocketClient * globalClient = NULL;
-void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len);
-
 
 boolean streaming = true;
 boolean sampling  = true;
-unsigned long prev_micros;
 size_t bytes_read;
 TaskHandle_t samplingTaskHandle;
 
 
-// Prototypes:
-void configure_i2s();
-static void getDataLoopTask(void * pvParameters);
-static const inline void sendSamples();
-static const inline void getSamples();
+volatile uint16_t latestRawSample = 0;    // último dado do buffer (atualizado em getSamples())
+volatile uint16_t filteredValue = 2048;   // valor filtrado (mesmo tipo do sinal original)
+volatile uint32_t lastFilterTime = 0;
+
+const uint32_t tau = 100 * 1000;        // tau em micros 
+const uint32_t T_filter = 1000;   // intervalo do filtro em micros
 
 
+uint16_t y_prev = 2048;        // valor inicial do filtro (meio ADC 12bit)
+uint32_t last_filter_time = 0;
 
-//////////////////////////   CONFIGURE I2S SAMPLING   ////////////////////////////////
 void configure_i2s(){
-  i2s_config_t i2s_config = 
-    {
-    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_ADC_BUILT_IN),  // I2S receive mode with ADC
-    .sample_rate = adc_sample_freq,                                               // set I2S ADC sample rate
-    .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,                                 // 16 bit I2S (even though ADC is 12 bit)
-    .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,                                 // handle adc data as single channel (right)
-    .communication_format = I2S_COMM_FORMAT_STAND_I2S,                            // I2S format
-    .intr_alloc_flags = 0,                                                        // 
-    .dma_buf_count = 4,                                                           // number of DMA buffers >=2 for fastness
-    .dma_buf_len = dma_buffer_len,                                                // number of samples per buffer
-    .use_apll = 0,                                                                // no Audio PLL - buggy and not well documented
+  i2s_config_t i2s_config = {
+    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_ADC_BUILT_IN),
+    .sample_rate = adc_sample_freq,
+    .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+    .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,
+    .communication_format = I2S_COMM_FORMAT_STAND_I2S,
+    .intr_alloc_flags = 0,
+    .dma_buf_count = 4,
+    .dma_buf_len = dma_buffer_len,
+    .use_apll = 0,
   };
   adc1_config_channel_atten(adc_channel, ADC_ATTEN_11db);
   adc1_config_width(ADC_WIDTH_12Bit);
@@ -78,16 +61,13 @@ void configure_i2s(){
   vTaskDelay(1000);
 }
 
-
-/////////////////////////   SERVER FUNCTIONS   /////////////////////////////////////
 void serverons(){
-
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
     while(!SPIFFS.exists("/index.html")){
       Serial.println("Error, /index.html is not onboard");
       delay(1000);
     }
-    Serial.println("trying to send index.html");  // troubleshooting core crash if file not onboard
+    Serial.println("trying to send index.html");
     AsyncWebServerResponse* response = request->beginResponse(SPIFFS, "/index.html", "text/html");
     response->addHeader("Access-Control-Max-Age", "10000");
     response->addHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
@@ -117,129 +97,123 @@ void serverons(){
   
   server.on("/Stop", HTTP_GET, [](AsyncWebServerRequest * request) {
     Serial.println("----STOP----");
-    Serial.println("stopping stream");
-    request->send(204);
     streaming = false;
     sampling = false;
+    request->send(204);
   });
   
   server.on("/Start", HTTP_GET, [](AsyncWebServerRequest * request) {
     Serial.println("----START----");
-    Serial.println("starting stream");
-    request->send(204);
-    sampling = true;
     streaming = true;
+    sampling = true;
+    request->send(204);
   });
-  
 }
 
+static const inline void getSamples();
+static const inline void sendSamples();
 
-//////////////////////////   SETUP   ////////////////////////////////////////
-void setup() {
-  delay(500);
-  Serial.begin(115200);
-  delay(5000);
-  Serial.println("Hello World!");
-  // Setup Wifi:
-  WiFi.begin(ssid, password);     //Connect to your WiFi router
-  Serial.println("");
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.print("Wifi connected. IP: ");
-  Serial.println(WiFi.localIP());
-  
-  configure_i2s();
-  SPIFFS.begin() ? Serial.println("SPIFFS.OK") : Serial.println("SPIFFS.FAIL");
-  
-
-  // Setup server:
-  server.begin();
-  serverons();
-  ws.onEvent(onWsEvent);
-  server.addHandler(&ws);
-  server.begin();
-  xTaskCreatePinnedToCore(getDataLoopTask, "getDataLoopTask", 80000, NULL, 0, &samplingTaskHandle, 0 );
-
-  prev_micros = micros();
-}
-
-
-
-//////////////////////////   MAIN LOOP   /////////////////////////////////
-// Handle infrastructural things in main loop.
-// Sampling is handled in separate tasks outside of this loop.
-void loop() {
-  // webSocket.loop();
-  vTaskDelay(1);
-}
-
-
-
-/////////////////////   SAMPLING TASK AND FUNCTIONS  ////////////////////
 static void getDataLoopTask(void * pvParameters){
   for( ;; ){
-    //vTaskDelay(1);          // REQUIRED TO RESET THE WATCH DOG TIMER IF WORKFLOW DOES NOT CONTAIN ANY OTHER DELAY
     if(sampling){
       getSamples();
-      delayMicroseconds(100); // needed less than 1-tick (1ms) delay
-      esp_task_wdt_reset();  // reset watchdog timer
+      delayMicroseconds(100);
+      esp_task_wdt_reset();
     } else {
-      vTaskDelay(1);  // resets watchdog with 1-tick (1ms) delay
+      vTaskDelay(1);
     }
   }
 }
 
 static const inline void getSamples(){
   i2s_read(i2s_port, (void*)i2s_read_buff, i2s_buffer_len*sizeof(uint16_t), &bytes_read, portMAX_DELAY);
-  if(streaming && I2S_EVENT_RX_DONE && bytes_read>0){
+  if(streaming && bytes_read>0){
+    latestRawSample = i2s_read_buff[(bytes_read/2) - 1] & 0x0FFF;
     sendSamples();
   }
 }
 
-static const inline void sendSamples(){
-  // unsigned long micros_now = micros();    // used for troubleshooting sampling rate
-  //Serial.println(micros_now-prev_micros);  // used for troubleshooting sampling rate
-  
-  //// Per esp32.com forum topic 11023, esp32 swaps even/odd samples,
-  ////   i.g. samples 0 1 2 3 4 5 are stored as 1 0 3 2 5 4 ..
-  ////   Have to deinterleave manually...
-  //// Also need to mask upper 4 bits which contain channel info (see gitter chat between me-no-dev and bzeeman)
-  for(int i=0; i<ws_tx_buffer_len; i+=2){  // caution: this is not robust to odd buffer lens
-    ws_send_buffer[i] = i2s_read_buff[i+1] & 0x0FFF;
-    ws_send_buffer[i+1] = i2s_read_buff[i] & 0x0FFF;
-    // Serial.println(ws_send_buffer[i]);
+// filtro discreto, usando diferença de tempo em micros
+// filtro discreto, usando diferença de tempo em micros
+void filterTask(void* pvParameters) {
+  unsigned long currentTime = micros();
+  lastFilterTime = currentTime;
+
+  for (;;) {
+    vTaskDelay(pdMS_TO_TICKS(T_filter / 1000)); // delay em ms
+
+    unsigned long now = micros();
+    unsigned long T = now - lastFilterTime;
+    lastFilterTime = now;
+
+    // filtro exponencial: y[n] = (tau * y[n-1] + T * x[n]) / (tau + T)
+    uint16_t y_prev = filteredValue;
+    uint16_t x = latestRawSample;
+
+    // Calcula com precisão float e converte para uint16_t
+    float result = (tau * y_prev + T * x) / (float)(tau + T);
+    filteredValue = (uint16_t)result;
   }
-  
-  // Serial.print("Samples: ");
-  // for(int i=0; i<10; i++){  // só os primeiros 10 para não lotar
-  //   Serial.print(ws_send_buffer[i]);
-  //   Serial.print(" ");
-  // }
-  // Serial.println();
+}
 
+static const inline void sendSamples(){
+  static uint16_t ws_send_buffer_filtered[ws_tx_buffer_len * 2];
 
-  // Send binary packet
-  ws.binaryAll((uint8_t*)ws_send_buffer, ws_tx_buffer_len * sizeof(uint16_t));
-  //prev_micros = micros_now;  // use for troubleshooting sampling rate
+  for(int i=0; i<ws_tx_buffer_len; i++){
+    uint16_t rawSample = i2s_read_buff[i] & 0x0FFF;
+    uint16_t filteredSample = filteredValue;  // mesmo tipo, sem cast necessário
+
+    ws_send_buffer_filtered[2*i] = rawSample;
+    ws_send_buffer_filtered[2*i + 1] = filteredSample;
+  }
+  ws.binaryAll((uint8_t*)ws_send_buffer_filtered, ws_tx_buffer_len * 2 * sizeof(uint16_t));
 }
 
 
+void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type,
+  void * arg, uint8_t *data, size_t len){
 
-
-void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len){
-  Serial.println("Async WebSocket event");
-  if(type == WS_EVT_CONNECT){
-    Serial.println("Async_WebSocket_Connected");
-    globalClient = client;
-  } else if(type == WS_EVT_DISCONNECT){
-    globalClient = NULL;
-    streaming = false;
-    sampling = false;
-    Serial.println("Async_WebSocket_Disconnected");
-  } else {
-    Serial.println("Unhandled Async WebSocket Event");
+  switch(type){
+    case WS_EVT_CONNECT:
+      Serial.println("WebSocket client connected");
+      globalClient = client;
+      break;
+    case WS_EVT_DISCONNECT:
+      Serial.println("WebSocket client disconnected");
+      globalClient = NULL;
+      break;
+    default:
+      break;
   }
+}
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected");
+
+  if(!SPIFFS.begin(true)){
+    Serial.println("SPIFFS mount failed");
+    return;
+  }
+
+  configure_i2s();
+
+  serverons();
+  ws.onEvent(onWsEvent);
+  server.addHandler(&ws);
+  server.begin();
+
+  xTaskCreatePinnedToCore(filterTask, "FilterTask", 4096, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(getDataLoopTask, "SampleLoop", 2048, NULL, 1, &samplingTaskHandle, 0);
+}
+
+void loop() {
+  esp_task_wdt_reset();
+  delay(1000);
 }
